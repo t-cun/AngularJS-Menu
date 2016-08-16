344ce307a0d426a7508dae162997d1590bf18a5f
Todd C <tcunningham07@gmail.com>
2016-08-15 18:42:42 -0700

    add gulp, use ng-show to hide/show details

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,12 +2,12 @@
 
 angular.module('confusionApp',[])
 
-        .controller('menuController', function() {
+        .controller('MenuController', ['$scope', function($scope) {
 
-          this.tab = 1;
-          this.filtText = '';
-
-          var dishes = [
+          $scope.tab = 1;
+          $scope.filtText = '';
+          $scope.showDetails = false;
+          $scope.dishes = [
           {
             name:'Uthapizza',
             image: 'images/uthapizza.png',
@@ -44,21 +44,24 @@ angular.module('confusionApp',[])
             description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
             comment: ''
           }];
-          this.dishes = dishes;
-          this.select = function(setTab) {
-            this.tab = setTab;
+
+          $scope.select = function(setTab) {
+            $scope.tab = setTab;
 
             if(setTab === 2) {
-              this.filtText = "appetizer";
+              $scope.filtText = "appetizer";
             } else if (setTab === 3) {
-              this.filtText = "mains";
+              $scope.filtText = "mains";
             } else if (setTab === 4) {
-              this.filtText = "dessert";
+              $scope.filtText = "dessert";
             } else {
-              this.filtText = "";
+              $scope.filtText = "";
             }
-          };          
-          this.isSelected = function(tab) {
-            return this.tab === tab;
           };
-        });
+          $scope.isSelected = function(tab) {
+            return $scope.tab === tab;
+          };
+          $scope.toggleDetails = function() {
+            $scope.showDetails = !$scope.showDetails;
+          };
+        }]);
